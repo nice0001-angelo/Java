72716eb1f99da4bf46820a639d2c95f6060629d6
nice0001-angelo <nice0001@gmail.com>
2019-11-13 18:36:44 +0900

    update

--- JT_DR_Loan/src/loan001/Genie001.java
+++ JT_DR_Loan/src/loan001/Genie001.java
@@ -5,8 +5,7 @@ public class Genie001 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I can do it");
-		int level;
-		level = 10;
+		int level = 10;
 		System.out.println(level);
 
 	}
