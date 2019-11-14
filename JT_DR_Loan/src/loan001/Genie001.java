20197ab20d0edcd9a580e49d6bf3cbd2368aee14
nice0001-angelo <nice0001@gmail.com>
2019-11-14 07:57:53 -0500

    update

--- JT_DR_Loan/src/loan001/Genie001.java
+++ JT_DR_Loan/src/loan001/Genie001.java
@@ -22,6 +22,23 @@ public class Genie001 {
 		System.out.println(num);
 		int result = ~num;
 		System.out.println(result);
+		
+		
+		int floor = 4;
+		
+		switch(floor) {
+		case 1 : System.out.println("1Ãþ ¾à±¹");
+				break;
+		case 2 : System.out.println("2Ãþ Á¤Çü¿Ü°ú");
+				break;
+		case 3 : System.out.println("3Ãþ ÇÇºÎ°ú");
+				break;
+		case 4 : System.out.println("4Ãþ Ä¡°ú");
+				break;
+        case 5 : System.out.println("5Ãþ Çï½ºÅ¬·³");
+				break;				
+		}
+		
 	}
 
 }
