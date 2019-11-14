9c6a20e3c10500325c16fc26cf1f17a569d5cf05
nice0001-angelo <nice0001@gmail.com>
2019-11-14 13:38:53 +0900

    update

--- JT_DR_Loan/src/loan001/Genie001.java
+++ JT_DR_Loan/src/loan001/Genie001.java
@@ -2,11 +2,14 @@ package loan001;
 
 public class Genie001 {
 
-
-	
 	public static void main(String[] args) {
 		char ch1 = 'A';
 		System.out.println(ch1);
+		System.out.println((int)ch1);
+		
+		int ch2 = 66;
+		System.out.println(ch2);
+		System.out.println((char)ch2);
 	}
 
 }
