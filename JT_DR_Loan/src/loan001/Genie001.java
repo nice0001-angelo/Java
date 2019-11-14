ead9e6a9fff33965852997fbcea22aba0caac734
nice0001-angelo <nice0001@gmail.com>
2019-11-14 13:37:03 +0900

    update

--- JT_DR_Loan/src/loan001/Genie001.java
+++ JT_DR_Loan/src/loan001/Genie001.java
@@ -2,12 +2,24 @@ package loan001;
 
 public class Genie001 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("I can do it");
-		int level = 10;
-		System.out.println(level);
 
+	
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
 	}
 
 }
+
+/*
+public class IntegerVariable {
+	
+	public static void main(String[] args) {
+		short sVal = 10;
+		byte bVal= 20;
+		System.out.println(sVal+bVal);
+	}
+	
+}
+*/
+
