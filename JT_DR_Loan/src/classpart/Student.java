22cdc47f906e28ed1cf7bb15fbd9911b3ee10ed2
nice0001-angelo <nice0001@gmail.com>
2019-11-14 10:27:53 -0500

    update

--- JT_DR_Loan/src/classpart/Student.java
+++ JT_DR_Loan/src/classpart/Student.java
@@ -1,5 +1,32 @@
 package classpart;
 
 public class Student {
-
+	
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo(){
+		System.out.println(studentName + "," + address);  // Name, Address Print
+		return;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}	
+	
+	public void setStudentName(String name){
+		studentName = name;
+		return;
+	}
+	
+	public static void main(String[] args) {
+		Student studentAhn = new Student();
+		studentAhn.studentName = "Angelo Nam"; 
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+		return;
+	}
 }
