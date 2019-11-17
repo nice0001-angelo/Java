983c35e65e5deed8c80a14df22d034bc471ed1d8
nice0001-angelo <nice0001@gmail.com>
2019-11-17 07:12:15 -0500

    update

--- transport/src/transport/Calculator.java
+++ transport/src/transport/Calculator.java
@@ -20,5 +20,22 @@ public class Calculator {
 		this.CalModelNum = CalModelNum;
 		this.CalOwner = CalOwner;
 	}
-
+	
+	void powerOn() {
+		System.out.println("Power on");
+	}
+	
+	int plus(int x, int y) {
+		int result = x + y;
+		return result;
+	}
+	
+	double divide(int x, int y) {
+		double result = (double)x/(double)y;
+		return result;
+	}
+	
+	void powerOff() {
+		System.out.println("Power off");
+	}
 }
