3d9c399625d9dda791c57b9066f59bc8628f21ca
nice0001-angelo <nice0001@gmail.com>
2019-11-17 10:18:32 -0500

    update

--- transport/src/transport/Calculator.java
+++ transport/src/transport/Calculator.java
@@ -6,36 +6,54 @@ public class Calculator {
 	int CalModelNum;
 	String CalOwner;
 	
+	//Conductor
 	public Calculator() {
-		
 	}
 	
+	//Conductor
 	public Calculator(String CalMaker, int CalModelNum) {
 		this.CalMaker = CalMaker;
 		this.CalModelNum = CalModelNum;
 	}
 	
+	//conductor
 	public Calculator(String CalMaker, int CalModelNum, String CanOwner) {
 		this.CalMaker = CalMaker;
 		this.CalModelNum = CalModelNum;
 		this.CalOwner = CalOwner;
 	}
 	
+	//Method powerOn()
 	void powerOn() {
 		System.out.println("Power on");
 	}
 	
+	//Method Plus()
 	int plus(int x, int y) {
 		int result = x + y;
 		return result;
 	}
 	
+	//Method divide()
 	double divide(int x, int y) {
 		double result = (double)x/(double)y;
 		return result;
 	}
 	
+	//Method powerOff()
 	void powerOff() {
 		System.out.println("Power off");
+	}
+	
+	//정사각형 넓이 계산 Method
+	double areaRectangle(double width) {
+		double result = width*width;
+		return result;
+	}
+	
+	//사각형 넓이 계산 Method(areaRectangel overloading)
+	double areaRectangle(double height, double width) {
+		double result = height*width;
+		return result;
 	}
 }
--- transport/src/transport/CalculatorExec.java
+++ transport/src/transport/CalculatorExec.java
@@ -1,7 +1,6 @@
 package transport;
 
 public class CalculatorExec {
-
 	public static void main(String[] args) {
 		Calculator mycal = new Calculator();
 		mycal.powerOn();
@@ -13,7 +12,13 @@ public class CalculatorExec {
 		System.out.println("Divided result:"+result2);
 		
 		mycal.powerOff();
-
+		
+		System.out.println("==========================");
+		
+		double result3 = mycal.areaRectangle(20);
+		System.out.print("정사각형의 넓이는:"+result3+"입니다\n");
+		
+		double result4 = mycal.areaRectangle(20,40);
+		System.out.print("직사각형의 넓이는:"+result4+"입니다");
 	}
-
 }
