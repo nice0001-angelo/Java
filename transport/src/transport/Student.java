b05f9a34bf1d27cd77daad7999509dd29e822ef9
nice0001-angelo <nice0001@gmail.com>
2019-11-20 09:59:36 -0500

    update

--- transport/src/transport/Student.java
+++ transport/src/transport/Student.java
@@ -1,5 +1,6 @@
 package transport;
 import java.util.ArrayList;
+import javax.security.auth.*;
 
 public class Student {
 	int studentID;
@@ -12,10 +13,10 @@ public class Student {
 	public Student() {
 	}
 	
-	Public Student(int studentID, String subjectName) {
-		this.studentID = new studentID;
-		this.subjectName = new subjectName;
-		subjectList = new ArrayList<Subject>;
+	public Student(int studentID, String studentName) {
+		this.studentID = studentID;
+		this.studentName = studentName;
+		subjectList = new ArrayList<Subject>();
 		
 	}
 	
@@ -36,5 +37,21 @@ public class Student {
 	
 	public void showInfo() {
 		System.out.println(studentName +"is get olly "+money+"Korean Won");
+	}
+	
+	public void addSubject(String name, int score) {
+		Subject subject = new Subject();
+		subject.setName(name);
+		subject.setScorePoint(score);
+		subjectList.add(subject);
+	}
+	
+	public void showStudentInfo() {
+		int total = 0;
+		for(Subject s : subjectList) {
+			total += s.getScorePoint();
+			System.out.println("학생 "+studentName+"의 "+s.getName()+"과목 성적은 "+s.getScorePoint()+"입니다");
+			}
+	System.out.println("학생 "+studentName+"의 총점은 "+total+"입니다");
 	}
 }
