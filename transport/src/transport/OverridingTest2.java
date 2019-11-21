aefd3a764876b2194e11a38d20c90ac481302f1d
nice0001-angelo <nice0001@gmail.com>
2019-11-21 17:37:48 +0900

    update

--- transport/src/transport/OverridingTest2.java
+++ transport/src/transport/OverridingTest2.java
@@ -8,7 +8,7 @@ public class OverridingTest2 {
 
 	public static void main(String[] args) {
 		Customer vc = new VIPCustomer(10030, "Angelo Nam", 2000);
-		vc.bonusPoint = 1000;
+		vc.bonusPoint = 1000; //VIPCustomer call
 		
 		System.out.println("Customer name is : "+vc.getCustomerName() +" and price is " + vc.calcPrice(10000));
 	}
