aa728eaa59d62a1b05dbe4ec4947b942fea591ea
nice0001-angelo <nice0001@gmail.com>
2019-11-21 13:06:54 +0900

    update

---
+++ transport/src/transport/Customer.java
@@ -0,0 +1,59 @@
+package transport;
+
+public class Customer {
+	
+	protected int customerID;
+	protected String customerName;
+	protected String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+	
+	public Customer()
+	{
+		customerGrade = "SILVER";
+		bonusRatio = 0.01;
+	//	System.out.println("Cusomer() ������ ȣ��");
+	}
+	
+	public Customer(int customerID, String customerName){
+		this.customerID = customerID;
+		this.customerName = customerName;
+		customerGrade = "SILVER";
+		bonusRatio = 0.01;
+	//	System.out.println("Cusomer(int, String) ������ ȣ��");
+	}
+	
+	public int calcPrice(int price){
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo(){
+		return "Customer Name is : " + customerName + " and Customer's Grade : "  + customerGrade + " Bouns Point is : " + bonusPoint;  
+	}
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}
+
+}
