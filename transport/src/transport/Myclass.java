7f55638c4c74ac99cda08edd81671f182bcebf5e
nice0001-angelo <nice0001@gmail.com>
2019-11-24 04:39:03 -0500

    update

---
+++ transport/src/transport/Myclass.java
@@ -0,0 +1,30 @@
+package trans;
+
+public class MyClass {
+	// field
+	RemoteControl rc = new Television();
+
+	// constructor
+	MyClass() {
+	}
+
+	// constroctor with parameter
+	MyClass(RemoteControl rc) {
+		this.rc = rc;
+		rc.turnOn();
+		rc.setVolume(5);
+	}
+
+	// local valuable
+	void methodA() {
+		RemoteControl rc = new Audio();
+		rc.turnOn();
+		rc.setVolume(5);
+	}
+
+		//method
+	void methodB(RemoteControl rc) {
+		rc.turnOn();
+		rc.setVolume(5);
+	}
+}
