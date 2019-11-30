df8f904f76cf4416ef467960511efec83e3e626b
nice0001-angelo <nice0001@gmail.com>
2019-11-30 12:32:49 -0500

    update

---
+++ transport/src/grade/Student.java
@@ -0,0 +1,11 @@
+package grade;
+
+import java.util.ArrayList;
+
+public class Student {
+	private int studetnId;
+	private String studentName;
+	private Subject majorSubject;
+	
+	private ArrayList<Score> scoreList = new ArrayList<Score>();
+}
