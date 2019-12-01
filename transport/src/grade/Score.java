929aadb45e7d4e362c40738c5ecba65151f80ce8
nice0001-angelo <nice0001@gmail.com>
2019-12-01 06:08:32 -0500

    update

--- transport/src/grade/Score.java
+++ transport/src/grade/Score.java
@@ -1,7 +1,20 @@
 package grade;
 
 public class Score {
+	//클래스 필드
 	int studentId; //학번
 	Subject subject;  //과목 
 	int point; //점수
+	
+	
+	//생성자
+	public Score(int studentId, Subject subject, int point) {
+		this.studentId = studentId;
+		this.subject = subject;
+		this.point = point;
+	}
+	
+	public int getStudentId() {
+		return studentId;
+	}
 }
