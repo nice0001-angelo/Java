a3a81cb3d941284ec73815b16b52b13617fa5b5d
nice0001-angelo <nice0001@gmail.com>
2019-12-01 10:25:28 -0500

    update

--- transport/src/school/report/GenerateGradeReport.java
+++ transport/src/school/report/GenerateGradeReport.java
@@ -1,5 +1,18 @@
 package school.report;
 
+import java.util.ArrayList;
+
+import grade.BasicEvaluation;
+import grade.MajorEvaluation;
+import grade.GradeEvaluation;
+
+import school.Student;
+import school.Subject;
+import school.Score;
+import school.School;
+
+import utils.Define;
+
 public class GenerateGradeReport {
 
 }
