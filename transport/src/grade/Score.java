27c9dfd6f6ad37745cbe8206fb7dc26212bff85e
nice0001-angelo <nice0001@gmail.com>
2019-12-01 06:13:29 -0500

    update

--- transport/src/grade/Score.java
+++ transport/src/grade/Score.java
@@ -18,4 +18,24 @@ public class Score {
 	public int getStudentId() {
 		return studentId;
 	}
+	
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	public Subject getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+	
+	public int getPoint() {
+		return point;
+	}
+	
+	public void setPoint(int point) {
+		this.point = point;
+	}
 }
