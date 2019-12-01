87847421437a9fbf5308892f159f75655ad4a384
nice0001-angelo <nice0001@gmail.com>
2019-12-01 04:53:59 -0500

    update

--- transport/src/grade/Student.java
+++ transport/src/grade/Student.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class Student {
 	
-	private int studetnId;         //학생 id
+	private int studetnId;         //학생 번호
 	private String studentName;    //학생 이름
-	private Subject majorSubject;  //전공과목
+	private Subject majorSubject;  //전공
 	
+	// 과목별 점수
 	private ArrayList<Score> scoreList = new ArrayList<Score>();
 	
+	//생성자 : 학생번호 학생명 전공
 	public Student(int studentId, String studentName, Subject majorSubject) {
 		this.studetnId = studetnId;
 		this.studentName = studentName;
