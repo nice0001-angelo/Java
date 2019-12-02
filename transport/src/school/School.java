0c7bb4d58cae561b39a3715020ea950d5ac4b324
nice0001-angelo <nice0001@gmail.com>
2019-12-02 09:57:33 -0500

    update

--- transport/src/school/School.java
+++ transport/src/school/School.java
@@ -7,7 +7,7 @@ public class School {
 	private static School instance = new School();
 	
 	private static String SCHOOL_NAME = "Good school";
-	private ArrayList<Student> sutdentList = new ArrayList<Student>(); //등록된 학생
+	private ArrayList<Student> studentList = new ArrayList<Student>(); //등록된 학생
 	private ArrayList<Subject> subjectList = new ArrayList<Subject>(); //과목 리스트
 	
 	private School() {}
