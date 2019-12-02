df67369af363b044decfeeeac771dd95034ba79c
nice0001-angelo <nice0001@gmail.com>
2019-12-02 09:54:29 -0500

    update

--- transport/src/school/School.java
+++ transport/src/school/School.java
@@ -3,5 +3,12 @@ package school;
 import java.util.ArrayList;
 
 public class School {
-
+	//singleton pattern
+	private static School instance = new School();
+	
+	private static String SCHOOL_NAME = "Good school";
+	private ArrayList<Student> sutdentList = new ArrayList<Student>(); //등록된 학생
+	private ArrayList<Subject> subjectList = new ArrayList<Subject>(); //과목 리스트
+	
+	private School() {}
 }
