2a5055d5f2da456c676de4db34b79b3e00e71281
nice0001-angelo <nice0001@gmail.com>
2019-12-03 17:34:30 +0900

    update

--- transport/src/test/TestMain.java
+++ transport/src/test/TestMain.java
@@ -9,7 +9,9 @@ import utils.Define;
 
 public class TestMain {
 
-	
+	School goodSchool = School.getInstnace();
+	Subject korean;
+	Subject math;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
