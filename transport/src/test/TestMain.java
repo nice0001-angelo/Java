15001a246df8cd00cab2649e10c900fde486c6c6
nice0001-angelo <nice0001@gmail.com>
2019-12-03 17:30:21 +0900

    update

--- transport/src/test/TestMain.java
+++ transport/src/test/TestMain.java
@@ -4,9 +4,13 @@ import school.School;
 import school.Score;
 import school.Student;
 import school.Subject;
+import school.report.*;
+import utils.Define;
 
 public class TestMain {
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
