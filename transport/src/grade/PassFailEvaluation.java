6dc3dd016240d4383102420e44c048da32e29ad6
nice0001-angelo <nice0001@gmail.com>
2019-12-05 15:33:00 +0900

    update

--- transport/src/grade/PassFailEvaluation.java
+++ transport/src/grade/PassFailEvaluation.java
@@ -1,5 +1,18 @@
 package grade;
 
-public class PassFailEvaluation {
+public class PassFailEvaluation implements GradeEvaluation {
+	
+	public String getGrade(int point) {
+		
+		String grade;
+		
+		if(point >= 70 && point <= 100) {
+			grade = "p";
+		} else {
+			grade ="F";
+		}
+		
+		return grade;
+	}
 
 }
