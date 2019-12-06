52bdb0c6569180dd90ad383b966d3b401788822c
nice0001-angelo <nice0001@gmail.com>
2019-12-06 14:18:45 +0900

    update

--- transport/src/school/report/GenerateGradeReport.java
+++ transport/src/school/report/GenerateGradeReport.java
@@ -68,7 +68,7 @@ public class GenerateGradeReport {
 	//학점 평가 클래스
 	GradeEvaluation[] gradeEvaluation = {new BasicEvaluation(), new MajorEvaluation(), new PassFailEvaluation()};
 	
-	System.out.println("GradeEvaluation: " + gradeEvaluation);
+//	System.out.println("GradeEvaluation: " + gradeEvaluation);
 	
 	for(int i=0; i<scoreList.size(); i++) {
 		Score score = scoreList.get(i);
@@ -79,7 +79,7 @@ public class GenerateGradeReport {
 			String grade;
 			
 			// 과목 Id 체크
-//			System.out.println("sub: " + subjectId);
+			System.out.println("sub: " + subjectId);
 		
 			
 			if(score.getSubject().getSubjectId() == majorId) { //필수과목인 경우
