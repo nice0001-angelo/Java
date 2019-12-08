8a2963b82f452702ed02c27740a3fc4d220edc13
nice0001-angelo <nice0001@gmail.com>
2019-12-08 07:44:15 -0500

    update

--- transport/src/school/School.java
+++ transport/src/school/School.java
@@ -1,5 +1,7 @@
 /*학교는 유일한 객체이므로 싱글톤 패턴으로 구현한다.
 즉 객체생성할 일이 없다는 이야기다 모두 private으로 세팅한다
+싱글톤 패턴은 객체를 여러개 생성할 필요가 없는 것이다
+가령 공장이 한개이고 공장의 생선 번호가 유일하려면 객체가 여러번 생성되면 안된다.
 */
 
 package school;
@@ -12,10 +14,7 @@ public class School {
 	// 정적 필드 선언 : 자신의 객체를 생성해서 초기화함
 	private static School instance = new School();
 	
-	private static String SCHOOL_NAME = "Good school";
-	private ArrayList<Student> studentList = new ArrayList<Student>(); //등록된 학생
-	private ArrayList<Subject> subjectList = new ArrayList<Subject>(); //과목 리스트
-	
+
 	//생성자 : 싱글톤 패턴은 아래와 같이 private 클래스() {}로 선언함
 	private School() {}
 	
@@ -27,7 +26,13 @@ public class School {
 			instance = new School();
 		return instance;
 	}
-
+    
+	// 싱글톤 필드가 아닌 일반필드 및 객체 선언...
+	private static String SCHOOL_NAME = "Good school";
+	private ArrayList<Student> studentList = new ArrayList<Student>(); //등록된 학생
+	private ArrayList<Subject> subjectList = new ArrayList<Subject>(); //과목 리스트
+	
+	
 	// addStudent
 	public void addStudent(Student student) {
 		studentList.add(student);
