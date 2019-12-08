25488d7d9c2e3cf5378b31842ec40c5d77d7bd6d
nice0001-angelo <nice0001@gmail.com>
2019-12-08 07:14:02 -0500

    update

--- transport/src/school/School.java
+++ transport/src/school/School.java
@@ -7,14 +7,16 @@ package school;
 import java.util.ArrayList;
 
 public class School {
+	
 	//singleton pattern
+	// 정적 필드 선언 : 자신의 객체를 생성해서 초기화함
 	private static School instance = new School();
 	
 	private static String SCHOOL_NAME = "Good school";
 	private ArrayList<Student> studentList = new ArrayList<Student>(); //등록된 학생
 	private ArrayList<Subject> subjectList = new ArrayList<Subject>(); //과목 리스트
 	
-	//생성자
+	//생성자 : 싱글톤 패턴은 아래와 같이 private 클래스() {}로 선언함
 	private School() {}
 	
 	
