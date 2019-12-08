ffe3c0cde102ae808afc342315971e03cd4bcd5c
nice0001-angelo <nice0001@gmail.com>
2019-12-08 05:54:13 -0500

    update

--- transport/src/grade/GradeEvaluation.java
+++ transport/src/grade/GradeEvaluation.java
@@ -1,5 +1,8 @@
 package grade;
 
+
+//여러평가방법이 있으므로 인터페이스로 정의함
+//BasicEvaluation(기본과목), MajorEvaluation(전공과목), PassFailEvaluation(Pass Fail 과목)
 public interface GradeEvaluation {
 	public abstract String getGrade(int point);
 }
